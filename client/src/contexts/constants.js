e82a39d2fccf2a64c2b44c62af5e6e77231f59b8
= <=>
2021-08-01 04:38:28 +0700

    fix-constants

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,6 +1,6 @@
 export const apiUrl =
     process.env.NODE_ENV !== 'production'
-        ? 'http://localhost:5000/api'
-        : 'someDeloyUrl';
+        ? 'https://localhost:5000/api'
+        : 'https://limitless-citadel-02405.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern';
